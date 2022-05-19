91542ad3d42f85c263b1c538689d47a7530bf5f2
venkat yashwanth <mudiliyashwanth@gmail.com>
2022-05-19 15:00:43 +0530

    react basics

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from "./components/Home";
+import Gallery from "./components/Gallery";
+import Contact from "./components/Contact";
+import About from "./components/About";
+import Nav from "./components/Nav";
+import Notfound from "./components/Notfound";
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <header>
+          <Nav/>
+        </header>
+        <section className="container">
+          <Routes>
+            <Route path="/" element={<Home/>}></Route>
+            <Route path="/about" element={<About/>}></Route>
+            <Route path="/gallery" element={<Gallery/>}></Route>
+            <Route path="/contact" element={<Contact/>}></Route>
+            <Route path="*" element={<Notfound/>}></Route>
+          </Routes>
+        </section>
+      </Router>
+    </>
   );
 }
 
